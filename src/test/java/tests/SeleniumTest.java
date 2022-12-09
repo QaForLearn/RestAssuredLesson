35d1b59f22ca063f4a8794410c210de7217711b6
poseidonchik <muha.rox@gmail.com>
2022-12-09 18:21:52 +0300

    Селенид

--- src/test/java/tests/SeleniumTest.java
+++ src/test/java/tests/SeleniumTest.java
@@ -1,25 +1,65 @@
 package tests;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Capabilities;
 
 import java.io.File;
 
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.value;
 
+
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class SeleniumTest {
+
+
+    String firstName = "Vladislav";
+    String lastName = "Ivanov";
+    String userEmail = "vladislav@ivanov.net";
+    String userNumber = "9999999999";
+    String subject = "Physics";
+    String currentAddress = "Pacific Ocean";
+
     @Test
     void seleniumTest(){
         open("https://demoqa.com/automation-practice-form");
-        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
-        $("[placeholder='First Name']").setValue("Владислав");
-        $("[placeholder='First Name']").shouldHave(value("Владислав"));
-        $("#dateOfBirthInput").clear();
-        $("#dateOfBirthInput").setValue("07 Dec 2022");
-        $("[for='gender-radio-1']").click();
-        $("#uploadPicture").uploadFile(new File(""));
+        $("#firstName").setValue(firstName); // вводим имя
+        $("#lastName").setValue(lastName); // вводим фамилию
+        $("#userEmail").setValue(userEmail); // вводим почту
+        $("#userNumber").setValue(userNumber); // вводим номер телефона
+        $("[id=subjectsInput]").setValue(subject).pressEnter(); // выбираем из списка
+        $("[id=currentAddress]").setValue(currentAddress).pressEnter(); // вводим адрес
+        $("[for=gender-radio-3]").click(); // идентифицируем себя как вертолет апач
+        $("[for=hobbies-checkbox-1]").click();
+        $("[for=hobbies-checkbox-2]").click();
+        $("[for=hobbies-checkbox-3]").click(); // выбираем радиобаттоны
+        $x("//input[@id=\"dateOfBirthInput\"]").click();
+        $(".react-datepicker__year-select").selectOption("1900");
+        $(".react-datepicker__month-select").selectOption("January");
+        $(".react-datepicker__day--001").click(); // выбираем год, месяц, день в календаре
+        $(byText("Select State")).click();
+        $(byText("Uttar Pradesh")).click();
+        $(byText("Select City")).click();
+        $(byText("Agra")).click(); // выбираем штат и город из выпадающих списков
+        $("#uploadPicture").uploadFile(new File("src/test/resources/harold.jpg")); // загружаем файл
+        $("[id=submit]").click(); // кликаем на кнопку
+        $(".table-responsive").shouldHave(
+                text("Student name"), text(firstName + ' ' + lastName),
+                text("Student Email"), text(userEmail),
+                text("Gender"), text("Other"),
+                text("Mobile"), text(userNumber),
+                text("Date Of Birth"), text("01 January,1900"),
+                text("Subject"), text(subject),
+                text("Address"), text(currentAddress),
+                text("Hobbies"), text("Sports, Reading, Music"),
+                text("Picture"), text("harold.jpg"),
+                text("State and City"), text("Uttar Pradesh Agra")
+        ); // проверяем форму
     }
 }
